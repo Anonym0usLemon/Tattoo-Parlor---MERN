75097ed2dbf76cc99c8c8ac5cf8654e7b0785bc2
Dominic DiModugno <anonym0uslemon@Dominics-Laptop.nc3pt0r.com>
2023-07-17 18:31:41 -0500

    no JQuery, FAQ page complete

--- client/src/components/FAQ.js
+++ client/src/components/FAQ.js
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+//Here's a cool example of how React props work :D 
+
+import React, { useState } from 'react';
 
 const FAQ = () => {
     return (
@@ -27,101 +29,35 @@ const FAQ = () => {
             <img className="faq-bird" src="/images/FAQs/bird.png" alt=""/>
 
             <ul className="faq-list FAQList">
-                
-                <li className="faq-question1 FAQEntry"  id="" title="">
-                    <h2 className="js-FAQH FAQEntry__heading"><span className="faq-plus"></span><span className="faq-spacer"></span>
-                    
-                    <a href="">I haven't heard from my artist since I booked...should I still show up?</a></h2>
-
-                    <div className="FAQEntry__body">
-                        <div className="FAQEntry__bodyPos group">
-                            Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et 
-                            velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam 
-                            voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?
-                        </div>
-                    </div>
-                </li>
-
-
-                <li className="faq-question1 FAQEntry"  id="" title="">
-                    <h2 className="js-FAQH FAQEntry__heading"><span className="faq-plus"></span><span className="faq-spacer"></span>
-                    
-                    <a href="">Are Tattoos Safe?</a></h2>
-
-                    <div className="FAQEntry__body">
-                        <div className="FAQEntry__bodyPos group">
-                            Yes, as long as you go to a reputable tattoo artist that is following all recommended safety 
-                            precautions. Make sure you’re completely honest with your tattoo artist regarding any medical 
-                            condition you may have or if prone to fainting. We always recommend getting a good night sleep, 
-                            having a full meal and some sugar before getting a tattoo to alleviate dizziness, nausea, and to 
-                            increase your pain tolerance. Getting a tattoo with a low blood sugar level is not ideal.
-                        </div>
-                    </div>
-                </li>
-
-
-                <li className="faq-question1 FAQEntry"  id="" title="">
-                    <h2 className="js-FAQH FAQEntry__heading"><span className="faq-plus"></span><span className="faq-spacer"></span>
-                    
-                    <a href="">Do Tattoos Hurt?</a></h2>
-
-                    <div className="FAQEntry__body">
-                        <div className="FAQEntry__bodyPos group">
-                            Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et 
-                            velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam 
-                            voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?
-                        </div>
-                    </div>
-                </li>
-
-
-                <li className="faq-question1 FAQEntry"  id="" title="">
-                    <h2 className="js-FAQH FAQEntry__heading"><span className="faq-plus"></span><span className="faq-spacer"></span>
-                    
-                    <a href="">Can I bring a Friend/Family for moral support?</a></h2>
-
-                    <div className="FAQEntry__body">
-                        <div className="FAQEntry__bodyPos group">
-                            Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et 
-                            velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam 
-                            voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?
-                        </div>
-                    </div>
-                </li>
-
-
-                <li className="faq-question1 FAQEntry"  id="" title="">
-                    <h2 className="js-FAQH FAQEntry__heading"><span className="faq-plus"></span><span className="faq-spacer"></span>
-                    
-                    <a href="">How do I take care of my tattoo afterwards?</a></h2>
-
-                    <div className="FAQEntry__body">
-                        <div className="FAQEntry__bodyPos group">
-                            Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et 
-                            velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam 
-                            voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?
-                        </div>
-                    </div>
-                </li>
-
-
-                <li className="faq-question1 FAQEntry"  id="" title="">
-                    <h2 className="js-FAQH FAQEntry__heading"><span className="faq-plus"></span><span className="faq-spacer"></span>
-                    
-                    <a href="">My Ink Still Hasn't Healed...What Gives?</a></h2>
-
-                    <div className="FAQEntry__body">
-                        <div className="FAQEntry__bodyPos group">
-                            Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et 
-                            velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam 
-                            voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?
-                        </div>
-                    </div>
-                </li>
-
+                <FAQitem question={"I haven't heard from my artist since I booked...should I still show up?"} answer={"Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?"} /> 
+                <FAQitem question={"Are Tattoos Safe?"} answer={"Yes, as long as you go to a reputable tattoo artist that is following all recommended safety precautions. Make sure you’re completely honest with your tattoo artist regarding any medical condition you may have or if prone to fainting. We always recommend getting a good night sleep, having a full meal and some sugar before getting a tattoo to alleviate dizziness, nausea, and to increase your pain tolerance. Getting a tattoo with a low blood sugar level is not ideal."}/> 
+                <FAQitem question={"Do Tattoos Hurt?"} answer={"Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?"} /> 
+                <FAQitem question={"Can I bring a friend/family member for moral support?"} answer={"Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?"} /> 
+                <FAQitem question={"How do I take care of my tattoo afterwards?"} answer={"Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?"} /> 
+                <FAQitem question={"My ink still hasn't healed...what gives?"} answer={"Et possimus autem sed beatae voluptatem et temporibus commodi aut fugit similique. Aut quia nihil et velit commodi aut sint nostrum quo dignissimos praesentium et ullam harum hic molestiae possimus. Nam voluptatem modi et autem laborum aut laudantium molestias ea nihil cumque?"} /> 
             </ul>
         </div>
     </section>
+    );
+}
+
+const FAQitem = (props) => {
+
+    const [open, setOpen] = useState(false); 
+
+    return (
+        <>
+            <li className="faq-question FAQEntry"  id="" title="" onClick={() => {setOpen(!open)}}>
+                <h2 className="js-FAQH FAQEntry__heading">
+                    <span className={`faq-plus ${open? 'plus-active' : 'plus-inactive'}`}></span><span className="faq-spacer"></span>
+                    {props.question}
+                </h2>
+
+                    <div className={`FAQEntry__bodyPos group ${open? 'active' : 'inactive'}`}>
+                        {props.answer}
+                    </div>
+            </li>
+        </>
     );
 }
 
